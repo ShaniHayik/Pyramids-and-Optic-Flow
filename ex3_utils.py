f824fdc9259dbf4c7de9e7ce703b24427514283e
Shani Hayik <shanihayik@gmail.com>
2020-06-24 03:00:27 +0300

    Add files via upload

--- ex3_utils.py
+++ ex3_utils.py
@@ -7,6 +7,7 @@ import cv2
 import scipy
 from numpy.linalg import LinAlgError
 import matplotlib.pyplot as plt
+from scipy.interpolate import RectBivariateSpline
 from scipy.ndimage.filters import convolve as sconvolve
 MIN_DIM = 16
 COLS = 1
@@ -23,38 +24,45 @@ RED = 0
 GREEN = 1
 
 
+def opticalFlow(old_frame_pyramid, new_frame_pyramid, r, ksize = 17):
+  pass
 
-def opticalFlow(im1: np.ndarray, im2: np.ndarray, step_size=10, win_size=5) -> (np.ndarray, np.ndarray):
-    kernel = [1, -1]
-    ans_uv = []
-    ans_xy = []
-    Ix = cv2.filter2D(im1, -1, kernel)
-    Iy = cv2.filter2D(im1.T, -1, kernel).T
-    It = im1 - im2
-    IxIx = np.multiply(Ix, Ix)
-    IyIy = np.multiply(Iy, Iy)
-    IyIx = np.multiply(Iy, Ix)
-    IxIt = np.multiply(Ix, It)
-    IyIt = np.multiply(Iy, It)
-
-    for i in range(np.shape(im1)[0]):
-        for j in range(np.shape(im1)[1]):
-            sum_IxIx = sumWindow(IxIx, i, j, win_size)
-            sum_IyIy = sumWindow(IyIy, i, j, win_size)
-            sum_IyIx = sumWindow(IyIx, i, j, win_size)
-
-            mat = np.array([[sum_IxIx, sum_IyIx], [sum_IyIx, sum_IyIy]])
-            l1, l2 = np.sort(np.linalg.eig(mat)[0])
-            if (l2 > 1 and l1/l2 < 100):
-                mat_inv = np.linalg.inv(mat)
-                sum_IxIt = -sumWindow(IxIt, i, j, win_size)
-                sum_IyIt = -sumWindow(IyIt, i, j, win_size)
-                mat2 = np.array([sum_IxIt, sum_IyIt])
-                mat_uv = mat_inv*mat2
-                ans_uv.append(list(mat_uv))
-                ans_xy.append([i,j])
-
-    return ans_xy,ans_uv
+#def opticalFlow(im1: np.ndarray, im2: np.ndarray, step_size=10, win_size=5) -> (np.ndarray, np.ndarray):
+    # kernel = [1, -1]
+    # kernel = np.array(kernel)
+    # ans_uv = []
+    # ans_xy = []
+    # Ix = cv2.filter2D(im1, -1, kernel)
+    # Iy = cv2.filter2D(im1.T, -1, kernel).T
+    # It = im1 - im2
+    # IxIx = np.multiply(Ix, Ix)
+    # IyIy = np.multiply(Iy, Iy)
+    # IyIx = np.multiply(Iy, Ix)
+    # IxIt = np.multiply(Ix, It)
+    # IyIt = np.multiply(Iy, It)
+    #
+    # for i in range(np.shape(im1)[0]):
+    #     for j in range(np.shape(im1)[1]):
+    #         sum_IxIx = sumWindow(IxIx, i, j, win_size)
+    #         sum_IyIy = sumWindow(IyIy, i, j, win_size)
+    #         sum_IyIx = sumWindow(IyIx, i, j, win_size)
+    #
+    #         mat = np.array([[sum_IxIx, sum_IyIx], [sum_IyIx, sum_IyIy]])
+    #         z = numpy.zeros((max(mat.shape), max(mat.shape), max(mat.shape)))
+    #         z[:mat.shape[0], :mat.shape[1]] = mat
+    #         mat = z
+    #         l1 = np.sort(np.linalg.eig(mat)[0])[0]
+    #         l2 = np.sort(np.linalg.eig(mat)[0])[1]
+    #         if (l2[0] > 1 and l1[0]/l2[0] < 100):
+    #             mat_inv = np.linalg.inv(mat)
+    #             sum_IxIt = -sumWindow(IxIt, i, j, win_size)
+    #             sum_IyIt = -sumWindow(IyIt, i, j, win_size)
+    #             mat2 = np.array([sum_IxIt, sum_IyIt])
+    #             mat_uv = mat_inv*mat2
+    #             ans_uv.append(list(mat_uv))
+    #             ans_xy.append([i,j])
+    #
+    # return ans_xy,ans_uv
 
 
 def get_window(x, y, win_size):
@@ -66,7 +74,9 @@ def get_window(x, y, win_size):
 
 def sumWindow (arr: np.ndarray, x: int, y: int, win_size: int):
     window = get_window(x,y,win_size)
-    sum = np.sum(arr[window])
+    for row in range(len(window) - 1):
+        for col in range(len(window[0]) - 1):
+            sum =+ arr[row,col]
     return sum
 
 
@@ -118,18 +128,11 @@ def get_filter(filter_size):
 def reduce(im, filter_vec):
     # filter_vec = np.zeros(shape=(130, 130), dtype=np.float32)
     # filter_vec = filter_vec[:, :, None]
-    blurred = blur_gus(im, filter_vec)
+    blurred = blur(im, filter_vec)
     return blurred[::2, ::2]
 
 #not mine
-def blur_gus(im, filter_vec):
-    #filter_vec = np.reshape(filter_vec, (130,130))
-    filter_vec = filter_vec[:, :, None]
-    row_blurred = sconvolve(im, filter_vec, mode='nearest')
-    col_blurred = sconvolve(np.transpose(row_blurred), filter_vec)
-    return np.transpose(col_blurred)
-
-def blur_lup(im, filter_vec):
+def blur(im, filter_vec):
     #filter_vec = np.reshape(filter_vec, (130,130))
     filter_vec = filter_vec[:, :, None]
     row_blurred = sconvolve(im, filter_vec, mode='nearest')
@@ -140,7 +143,7 @@ def blur_lup(im, filter_vec):
 def gaussExpand(img: np.ndarray, gs_k: np.ndarray) -> np.ndarray:
     arr = np.zeros((img.shape[0] * 2, img.shape[1] * 2))
     arr[0:-1:2][0:-1:2] = np.copy(img)
-    arr = blur_gus(arr, gs_k)
+    arr = blur(arr, gs_k)
     return arr
 
 
@@ -159,14 +162,20 @@ def laplaceianReduce(img: np.ndarray, levels: int = 4) -> List[np.ndarray]:
 def expand(im, shape, filter_vec):
     zeros = np.zeros(shape)
     zeros[::2, ::2] += im
-    return blur_lup(zeros, 2*filter_vec)
+    return blur(zeros, 2*filter_vec)
 
 #Return the original image from a laplacian pyramid
 def laplaceianExpand(lap_pyr: List[np.ndarray], coeff, filter_vec) -> np.ndarray:
-    lpyr = [lap_pyr[i] * coeff[i] for i in range(len(lap_pyr))]
-    for i in range(len(lpyr) - 2, -1, -1):
-        lpyr[i] = lpyr[i] + expand_im(lpyr[i + 1], filter_vec)
-    return lpyr[i]
+    for i in range(len(coeff)):
+        lap_pyr[i] *= coeff[i]
+
+    image = lap_pyr[SMALLEST_PYR]
+    for i in range(len(lap_pyr) - 1, LARGEST_PYR, REVERSED):
+        image = expand(image, lap_pyr[i - 1].shape, filter_vec) + lap_pyr[i - 1]
+
+    return image
+
+
 
 def expand_im(im, g_kernel):
     """
@@ -177,7 +186,7 @@ def expand_im(im, g_kernel):
     """
     padded_im = np.zeros((im.shape[0] * 2, im.shape[1] * 2, im.shape[2]), dtype=float)
     padded_im[::2, ::2] = im.copy()
-    toReturn = blur_lup(padded_im, g_kernel * 2)
+    toReturn = blur(padded_im, g_kernel * 2)
     return toReturn
 
 def blur_image(im, g_filter):
@@ -204,4 +213,5 @@ def pyrBlend(img_1: np.ndarray, img_2: np.ndarray, mask: np.ndarray, levels: int
     coeff = [1] * len(lapl_pyr2)
     filter_vec = get_filter(10)
     blend_im = laplaceianExpand(Lout, coeff, filter_vec)
-    return blend_im.clip(0, 1)
+    blur = blur_image(img_1, filter_vec)
+    return blend_im.clip(0, 1), blur
